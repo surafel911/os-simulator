7695fd6456fa1eb98b5e2ca50c830b893e9bd8c8
Surafel Assefa <surafel911@gmail.com>
2020-04-19 17:54:17 -0400

    Renames files, adds error header to log.

--- src/driver.c
+++ src/driver.c
@@ -1,14 +1,17 @@
 #include <os-simulator/driver.h>
 
+#include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
 
 void
 driver_error(const char* format, ...)
 {
 	va_list args;
 
-	va_start(args);
+	va_start(args, format);
 
+	fprintf(stderr, "ERROR: ");
 	vfprintf(stderr, format, args);
 
 	va_end(args);
