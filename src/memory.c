23c4f4e3bc0570ea1fb36ea0e1afcddd90a3db40
Surafel Assefa <surafel911@gmail.com>
2020-04-19 17:54:47 -0400

    Adds bounds checking.

--- src/memory.c
+++ src/memory.c
@@ -2,6 +2,8 @@
 
 #include <stdint.h>
 
+#include <os-simulator/driver.h>
+
 #define MEMORY_RAM_SIZE		1024
 #define MEMORY_DISK_SIZE	2048
 
@@ -10,23 +12,39 @@ static uint32_t _ram[MEMORY_RAM_SIZE], _disk[MEMORY_DISK_SIZE];
 uint32_t
 memory_ram_get(uint16_t addr)
 {
+	if (addr > MEMORY_RAM_SIZE) {
+		driver_error("RAM out of bounds access.");
+	}
+
 	return _ram[addr];
 }
 
 void
 memory_ram_set(uint16_t addr, uint32_t word)
 {
+	if (addr > MEMORY_RAM_SIZE) {
+		driver_error("RAM out of bounds access.");
+	}
+
 	_ram[addr] = word;
 }
 
 uint32_t
 memory_disk_get(uint16_t addr)
 {
+	if (addr > MEMORY_DISK_SIZE) {
+		driver_error("DISK out of bounds access.");
+	}
+
 	return _disk[addr];
 }
 
 void
 memory_disk_set(uint16_t addr, uint32_t word)
 {
+	if (addr > MEMORY_DISK_SIZE) {
+		driver_error("DISK out of bounds access.");
+	}
+
 	_disk[addr] = word;
 }
